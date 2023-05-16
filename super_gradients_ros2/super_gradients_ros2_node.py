18d8c4bf3742f1a656f4ec6deac6643952f66fc4
fgonzalezr1998 <fergonzaramos@yahoo.es>
2023-05-17 01:16:50 +0200

    [WIP] sensor_msgs/Image publication. Improvements needed

--- super_gradients_ros2/super_gradients_ros2_node.py
+++ super_gradients_ros2/super_gradients_ros2_node.py
@@ -42,13 +42,17 @@ class SuperGradientsNode(Node):
                 ('node_rate', 10),
                 ('camera_topic', '/camera_topic'),
                 ('min_prob', 0.2),
-                ('bboxes_topic', '/sg_ros2/bboxes')
+                ('bboxes_topic', '/sg_ros2/bboxes'),
+                ('publish_image', True),
+                ('image_topic', '/sg_ros2/image')
             ]
         )
         self._node_rate = self.get_parameter('node_rate').get_parameter_value().double_value
         self._camera_topic = self.get_parameter('camera_topic').get_parameter_value().string_value
         self._min_prob = self.get_parameter('min_prob').get_parameter_value().double_value
         self._bboxes_topic = self.get_parameter('bboxes_topic').get_parameter_value().string_value
+        self._publish_image = self.get_parameter('publish_image').get_parameter_value().bool_value
+        self._image_topic = self.get_parameter('image_topic').get_parameter_value().string_value
         if ((self._min_prob < 0.0) or (self._min_prob > 1.0)):
             self._status.status = self._status.STATUS_ERR
             self._status.set_msg("Minimum Probability must be between 0.0 and 1.0")
@@ -72,6 +76,9 @@ class SuperGradientsNode(Node):
         
         self._bboxes_pub = self.create_publisher(
             Detection2DArray, self._bboxes_topic, qos)
+
+        self._image_pub = self.create_publisher(
+            Image, self._image_topic, qos)
     
     def _show_image(self, image: np.ndarray) -> None:
         """Show an image using matplotlib.
@@ -92,7 +99,7 @@ class SuperGradientsNode(Node):
         if (not self._image_received):
             self._image_received = True
     
-    def _publish_bboxes(self, predictions, raw_img):
+    def _publish_bboxes(self, predictions, raw_img, img_cv2):
         detection_arr = Detection2DArray()
 
         # Set the Header
@@ -110,10 +117,17 @@ class SuperGradientsNode(Node):
                 result.score = conf.item()
                 detection.results.append(result)
 
-                detection.bbox.center.x = (bbox[2].item() + bbox[0].item()) /  2.0
-                detection.bbox.center.y = (bbox[3].item() + bbox[1].item()) / 2.0
-                detection.bbox.size_x = bbox[2].item() - bbox[0].item()
-                detection.bbox.size_y = bbox[3].item() - bbox[1].item()
+                detection.bbox.center.x = float(int((bbox[2].item() + bbox[0].item()) /  2))
+                detection.bbox.center.y = float(int((bbox[3].item() + bbox[1].item()) / 2))
+                detection.bbox.size_x = float(int(bbox[2].item()) - int(bbox[0].item()))
+                detection.bbox.size_y = float(int(bbox[3].item()) - int(bbox[1].item()))
+                if (self._publish_image):
+                  cv2.rectangle(img_cv2,
+                                (int(bbox[0].item()), int(bbox[1].item())),
+                                (int(bbox[2].item()), int(bbox[3].item())), (255,0,0), 3)
+                  cv2.putText(img_cv2, result.id + " " + str(round(result.score, 2)),
+                              (int(bbox[0].item()), int(bbox[1].item())),
+                              cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
 
                 detection.source_img = raw_img
                 detection.is_tracking = False
@@ -121,17 +135,24 @@ class SuperGradientsNode(Node):
                 detection_arr.detections.append(detection)
         self._bboxes_pub.publish(detection_arr)
 
+        # Publish the image
+        if (self._publish_image):
+            img = Image()
+            img = self._cv_bridge.cv2_to_imgmsg(img_cv2)
+            img.header.frame_id = 'camera_link'
+            self._image_pub.publish(img)
+
     def _step(self):
         if (self._status.is_ok()):
             if (self._image_received):
                 raw_img = self._last_img_msg
 
-                img = self._cv_bridge.imgmsg_to_cv2(self._last_img_msg)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img_cv2 = self._cv_bridge.imgmsg_to_cv2(self._last_img_msg)
+                img = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2RGB)
 
                 predictions = self._model.predict(img, conf=self._min_prob)
 
-                self._publish_bboxes(predictions, raw_img)
+                self._publish_bboxes(predictions, raw_img, img_cv2)
         else:
             self.get_logger().error(self._status.msg)
 
